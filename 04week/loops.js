84373da19208b08df2d09276f7e9de389acfa2b1
KirbyPatrick <pkirbyinatx@gmail.com>
2018-08-19 11:36:07 -0500

    loops

---
+++ 04week/loops.js
@@ -0,0 +1,39 @@
+const carsInReverse = ['Toyota', 'Tesla', 'GMC', 'Honda']
+
+for(let i = 0; i < carsInReverse.length; i++){
+  console.log(carsInReverse[i])
+};
+
+//skipping the for... in loop questions because I don't understand them and they aren't covered in the course work.  plus Renee said I shouldn't bother.
+
+const persons = {
+  firstName: "Jane",
+  lastName: "Doe",
+  birthDate: "Jan 5, 1925",
+  gender: "female"
+};
+
+
+
+for(i = 1; i <= 100; i++){
+  console.log(i)
+}
+
+
+// // while loop -- Use a while loop to console.log the numbers 1 to 1000.
+
+let i = 0;
+while (i < 100){
+  i++;
+  console.log(i)
+};
+
+
+// // do...while loop -- Use a do...while loop to console.log the numbers from 1 to 1000.
+
+let x = 0;
+do {
+  x+= 1;
+  console.log(x);
+} while(x<100);
+
